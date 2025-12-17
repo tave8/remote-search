52d5056921a9c32b1178c96089b237361a54ebc9
Giuseppe Tavella <giuseppetavella8@gmail.com>
2025-12-17 23:12:00 +0100

    organize files

--- dist/script.js
+++ dist/script.js
@@ -1,14 +1,10 @@
-// TO IMPLEMENT
-// when user clicks out of list input, or when input loses focus, list disappears
-// on window resize, set a timeout mechanism with clear last timeout and set new timeout, to reposition the list underneath the input
-// when click item, fill an input value
-// when click item,
 
-// Only array of objects are accepted.
-// So [
-// {myLabel: "value"}
-// ]
-
+/**
+ * Remote search, made easy.
+ * Configure your search in a few lines, so you can focus on your app's core logic.
+ * 
+ * @author Giuseppe Tavella 
+ */
 class RemoteSearch {
   constructor({
     inputSelector,
@@ -419,69 +415,3 @@ class RemoteSearch {
   }
 }
 
-// USAGE
-
-new RemoteSearch({
-  // where the user types
-  inputSelector: ".remote-search-box > .input-box > input",
-  // where results are shown
-  // listSelector: ".box-search > .list",
-  // min char number to trigger remote search
-  minLen: 3,
-  // url to make request to
-  absoluteUrl: "https://jsonplaceholder.typicode.com/users",
-  // when user clicks the individual result item
-  onClickItem: async (item) => {
-    console.log("clicked item", item);
-  },
-  // when the result is received from the server, return the actual items (list of objects)
-  getItemsFromResult: (responseData) => {
-    // in this case, the items are found exactly in the json itself,
-    // so there's no need to search any further
-    // if instead the items are found in the json.items property, you must
-    // specify that here with return json.items
-    return responseData;
-  },
-  // when the results arrive to the client, from the server
-  onGetResults: async (responseData, responseObj) => {
-    console.log("results arrived", responseData, responseObj);
-  },
-  // the property that will be displayed to the user in the result item
-  itemLabel: "name",
-  // you can customize the item label. this function will be called
-  // for each item, passing in the item. you can then set custom logic
-  // to display whichever label you want. if this function is not provided,
-  // the instance.itemLabel will be used
-  setCustomItemLabel: (item) => {
-    if (item.email.endsWith("biz")) {
-      return `${item.website} - ${item.name}`;
-    }
-    return item.name;
-  },
-  // visually mark/highlight the matching search text, in whatever the final item label will be?
-  highlightMatch: true,
-  // the search term query string parameter that will contain the value of the input
-  searchQueryParam: "term",
-  // the query params to append to url before making request
-  urlQueryParams: {
-    x: 2,
-  },
-  // the input placeholder, which can be dynamic as well
-  inputPlaceholder: "Search all (min _N_ required)",
-  // when the focus is lost on the search input, hide the result list?
-  onLoseFocusHideResultList: false,
-  // custom function to programmatically position the spinner wherever you want,
-  // using as reference point the search input
-  // positionSpinner: () => {
-  //   return {
-  //     top: -25,
-  //     left: 270,
-  //   };
-  // },
-  // positionResultList: () => {
-  //   return {
-  //     top: 0,
-  //     left: 0,
-  //   };
-  // },
-});
